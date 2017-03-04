7f22d0dcdbbe30da08fa0014e99f023f7ee1c05f
roberveron <roberveron@gmail.com>
2017-03-04 17:08:13 -0300

    Prueba autenticacion

---
+++ autenticacion/js/inicializar-firebase.js
@@ -0,0 +1,18 @@
+// Initialize Firebase
+//var config = {
+//  apiKey: "AIzaSyCXhHDjuVSDHQxdRhc_CRwjtTzQNhV_6Mw",
+//  authDomain: "fir-3-curso.firebaseapp.com",
+//  databaseURL: "https://fir-3-curso.firebaseio.com",
+//  storageBucket: "fir-3-curso.appspot.com",
+//  messagingSenderId: "496003402473"
+//};
+//firebase.initializeApp(config);
+
+var config = {
+    apiKey: "AIzaSyB4YQz1MGiahDmzw-a1R9m6mW4xLaq2euk",
+    authDomain: "viaje-de-pollitos.firebaseapp.com",
+    databaseURL: "https://viaje-de-pollitos.firebaseio.com",
+    storageBucket: "viaje-de-pollitos.appspot.com",
+    messagingSenderId: "917225638328"
+  };
+  firebase.initializeApp(config);
